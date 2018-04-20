a6773e77736be4008e618e329dcfa83bb9acad0a
AnnFell <annelottef@gmail.com>
2018-04-20 10:20:54 +0200

    binary search with html input

--- binary_search/script.js
+++ binary_search/script.js
@@ -1,11 +1,45 @@
 'use strict';
 
-const numbersMax = 1000;
-const numbers = Array.from(Array(numbersMax), (e, i) => i + 1);
-const numberToGuess = Math.floor(Math.random() * (numbersMax + 1));
+let numbersMax;
+let numbers;
+let numberToGuess;
 
-console.log(numbers);
-console.log(numberToGuess);
+
+// console.log(numberToGuess);
+const inputMax = document.querySelector('input[name=max]');
+const inputNumberToGuess = document.querySelector('input[name=choice]');
+const button = document.querySelector('button');
+const inputDiv = document.querySelector('.input');
+const resultDiv = document.querySelector('.result');
+
+button.addEventListener('click', getValues);
+
+function getValues() {
+    numbersMax = Number(inputMax.value);
+    numberToGuess = Number(inputNumberToGuess.value);
+    numbers = Array.from(Array(numbersMax), (e, i) => i + 1);
+
+    if (numbersMax < 10 || numbersMax === 0) {
+        inputDiv.innerHTML += '<p><strong>Please pick a number larger than 10 to use as maximum<strong></p>';
+    } else if (numberToGuess < 1 || numberToGuess > numbersMax) {
+        inputDiv.innerHTML += '<p><strong>Please pick a number to guess that is between 1 and your maximum number<strong></p>';
+    } else {
+        howManyGuesses(numbersMax);
+        binarySearchNum(numberToGuess);
+    }
+}
+
+function howManyGuesses(number) {
+    let result = 0;
+    while (number > 1) {
+        number = number - (number / 2);
+        result++;
+    }
+    inputDiv.innerHTML = `<p>Thank you for your input: you want to let the computer find the number ${numberToGuess} 
+    between 1 and ${numbersMax}!<br>
+    Using binary search, it will take the computer at most ${result} guesses! <br>
+    Let's see what the result is...</p>`;
+}
 
 function binarySearchNum(target) {
     let min = 0;
@@ -16,19 +50,17 @@ function binarySearchNum(target) {
     while (numbers[guess] !== target) {
 
         guess = Math.floor((max - min) / 2) + min;
-        console.log(`The current guess is ${guess}`);
+        resultDiv.innerHTML += `<p>The computer guesses: ${numbers[guess]}</p>`;
 
         if (numbers[guess] > target) {
+            resultDiv.innerHTML += `<p>No, too high</p>`;
             max = guess - 1;
-            console.log(`No, too high`);
             numAttempts++;
-        } else {
+        } else if(numbers[guess] < target) {
+            resultDiv.innerHTML += `<p>No, too low</p>`;
             min = guess + 1;
-            console.log(`No, too low`);
             numAttempts++;
         }
     }
-    console.log(`The number to guess was: ${target} and our final (${numAttempts}x) guess was ${numbers[guess]}!`);
+    resultDiv.innerHTML += `<p><strong>The number to guess was: ${target} and our final (${numAttempts}x) guess was ${numbers[guess]}!</strong></p>`;
 }
-
-binarySearchNum(numberToGuess);
